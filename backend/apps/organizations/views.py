f9baedc2ebecb6f741a3033317352dc124dbb5a0
mattyray <mnraynor90@gmail.com>
2025-12-12 16:26:25 -0500

    BACKEND ROUGH

---
+++ backend/apps/organizations/views.py
@@ -0,0 +1,18 @@
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from core.permissions import IsAdmin
+from .models import Organization
+from .serializers import OrganizationSerializer
+
+
+class CurrentOrganizationView(generics.RetrieveUpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = OrganizationSerializer
+    
+    def get_object(self):
+        return self.request.organization
+    
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'PATCH']:
+            return [IsAuthenticated(), IsAdmin()]
+        return [IsAuthenticated()]
