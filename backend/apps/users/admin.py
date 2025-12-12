19959a8e89b3722b75fe5f83545e10e6a6d952cc
mattyray <mnraynor90@gmail.com>
2025-12-12 16:25:46 -0500

    BACKEND ROUGH

---
+++ backend/apps/users/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ['email', 'first_name', 'last_name', 'is_email_verified', 'is_staff']
+    list_filter = ['is_staff', 'is_superuser', 'is_email_verified']
+    search_fields = ['email', 'first_name', 'last_name']
+    ordering = ['email']
+    
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'phone')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_email_verified')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2'),
+        }),
+    )
