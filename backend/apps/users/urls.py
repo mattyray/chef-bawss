512b47ea49079a934d9023174e062d7d1a5d863e
mattyray <mnraynor90@gmail.com>
2025-12-12 18:55:41 -0500

    BACKEND ROUGH

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView, MeView, ChangePasswordView, LogoutView
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import (
+    RegisterView,
+    ThrottledTokenObtainPairView,
+    MeView,
+    ChangePasswordView,
+    LogoutView
+)
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', ThrottledTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('me/', MeView.as_view(), name='me'),
